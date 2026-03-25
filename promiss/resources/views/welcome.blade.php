ffb5942c9f1f86c19a17e2b0f0cda2b026a374c1
Bruno <brunokristian003@gmail.com>
2026-03-25 09:16:15 +0100

    feat: add sitemap.xml and enhance meta description in welcome page

--- promiss/resources/views/welcome.blade.php
+++ promiss/resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Promiss | Personálna a hostesingová agentúra</title>
+    <meta name="description" content="Promiss je personálna a hostesingová agentúra, ktorá poskytuje profesionálny personál pre rôzne eventy a podujatia. Naša agentúra sa zameriava na poskytovanie kvalitných služieb a zabezpečenie spokojnosti našich klientov. Sme tu, aby sme vám pomohli nájsť ten správny personál pre vaše potreby, či už ide o hostesky, promotérov, alebo iný personál pre vaše podujatie. Kontaktujte nás ešte dnes a zistite, ako vám môžeme pomôcť s vašimi personálnymi potrebami!">
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
